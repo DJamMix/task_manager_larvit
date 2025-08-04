b0941ea98353c470072e1c837e533137c49f1b5e
v.antropov <v.antropov@friflex.com>
2025-08-04 21:21:21 +0400

    Убраны анимации со страницы телеграмм привязки

--- resources/views/telegram_connect.blade.php
+++ resources/views/telegram_connect.blade.php
@@ -150,9 +150,6 @@
     }
 </style>
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-
 <script>
     function copyCode() {
         const codeInput = document.getElementById('telegramCode');
